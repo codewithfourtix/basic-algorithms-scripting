93c1a9385886da29c8f40009d9d368f5e60ddbd0
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-08-16 22:43:43 +0500

    algo8

---
+++ algo8.js
@@ -0,0 +1,10 @@
+function fibonacci(n) {
+  let sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+  return sequence;
+}
+
+// Example
+console.log(fibonacci(7)); // [0, 1, 1, 2, 3, 5, 8]

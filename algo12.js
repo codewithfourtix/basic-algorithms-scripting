253fef8faff2338249f4a641151ce479bd037da2
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-08-17 21:47:23 +0500

    algo13 added && algo12 M

--- algo12.js
+++ algo12.js
@@ -1,1 +1,24 @@
 // Problem: Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+function longestConsecutive(nums) {
+  const numSet = new Set(nums);
+  let longestStreak = 0;
+
+  for (let num of nums) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
+  }
+
+  return longestStreak;
+}
+
+// Example usage:
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // Output: 4
---
+++ algo13.js
@@ -0,0 +1,1 @@
+// Problem: Given a string s and a string p, return an array of all the start indices of p's anagrams in s.

585f9245c865faa84fbddb975a1d6f726130c5a0
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-08-17 21:42:23 +0500

    algo12 added && algo11

--- algo11.js
+++ algo11.js
@@ -1,1 +1,8 @@
 // Find the k-th largest element in an unsorted array.
+function findKthLargest(nums, k) {
+  nums.sort((a, b) => b - a);
+  return nums[k - 1];
+}
+
+// Example
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); // 5
---
+++ algo12.js
@@ -0,0 +1,1 @@
+// Problem: Given an unsorted array of integers, find the length of the longest consecutive elements sequence.

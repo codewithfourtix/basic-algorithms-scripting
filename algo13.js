74bbdcc7f93b34050a52bf6997c68f21ea515189
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-08-18 23:22:01 +0500

    algo13 Mod

--- algo13.js
+++ algo13.js
@@ -1,1 +1,26 @@
 // Problem: Given a string s and a string p, return an array of all the start indices of p's anagrams in s.
+function findAnagrams(s, p) {
+  const result = [];
+  const pCount = Array(26).fill(0);
+  const sCount = Array(26).fill(0);
+
+  for (let i = 0; i < p.length; i++) {
+    pCount[p.charCodeAt(i) - 97]++;
+    sCount[s.charCodeAt(i) - 97]++;
+  }
+
+  for (let i = 0; i < s.length - p.length + 1; i++) {
+    if (pCount.toString() === sCount.toString()) {
+      result.push(i);
+    }
+    sCount[s.charCodeAt(i) - 97]--;
+    if (i + p.length < s.length) {
+      sCount[s.charCodeAt(i + p.length) - 97]++;
+    }
+  }
+
+  return result;
+}
+
+// Example usage:
+console.log(findAnagrams("cbaebabacd", "abc")); // Output: [0, 6]

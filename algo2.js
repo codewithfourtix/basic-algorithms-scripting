61a982be10e3cdd4d726dbf502466c7318d8e26a
Ali Zulfiqar <codewithfourtix@gmail.com>
2024-08-21 20:59:09 +0500

    algo 2 Mod

--- algo2.js
+++ algo2.js
@@ -1,13 +1,12 @@
 // Problem: Truncate a string (first argument) if it is longer than the given maximum string length (second argument).
 //  Return the truncated string with a ... ending.
-
 function truncateString(str, num) {
-    if (str.length > num) {
-      return str.slice(0, num) + '...';
-    } else {
-      return str;
-    }
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
   }
-  
-  console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8)); 
-  // Output: "A-tisket..."
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+// Output: "A-tisket..."
